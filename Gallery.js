e10b2637a7992c589a473f5c94e8396a6abcaa19
SHULBHIGUPTA <92017517+SHULBHIGUPTA@users.noreply.github.com>
2022-02-14 07:45:40 +0530

    Add files via upload

---
+++ Gallery.js
@@ -0,0 +1,17 @@
+class Gallery extends React.Component {
+    render() {
+        return (
+            <div className="gallery-container">
+                <img src="https://images.pexels.com/photos/3244513/pexels-photo-3244513.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/572897/pexels-photo-572897.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/460672/pexels-photo-460672.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/1796715/pexels-photo-1796715.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/575362/pexels-photo-575362.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/1427579/pexels-photo-1427579.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/2031726/pexels-photo-2031726.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500" />
+                <img src="https://images.pexels.com/photos/802257/pexels-photo-802257.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+                <img src="https://images.pexels.com/photos/1556624/pexels-photo-1556624.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+            </div>
+        )
+    }
+}
